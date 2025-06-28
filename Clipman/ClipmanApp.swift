6db4a9f2e87bceb4c5b4e110a5c39f3171e2250e
conistuffwow <deatu013@gmail.com>
2025-06-28 02:57:49 -0400

    Initial commit

---
+++ Clipman/ClipmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipmanApp.swift
+//  Clipman
+//
+//  Created by Coni on 2025-06-28.
+//
+
+import SwiftUI
+
+@main
+struct ClipmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
